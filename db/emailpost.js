c67584dac4fd0d94c7715c4e4925582b3f4734ba
unknown <funpk4@gmail.com>
2021-05-16 07:38:22 +0530

    'submit'

--- db/emailpost.js
+++
@@ -1,9 +0,0 @@
-let mongoose=require('mongoose');
-//create schema (structure)
-let adminloginSchema = new mongoose.Schema({
-    email:{type:String},
-  });
-
-  //model
-  module.exports = mongoose.model('adminlogin', adminloginSchema);
-//end
